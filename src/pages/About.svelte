ab8f2904daa883d24c32b776526b0df56e8bd1d8
kahverengi <ahmet.duruer@yandex.com>
2020-05-29 22:53:44 +0300

    Update about me text

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,11 +1,10 @@
-
 <h4 class="pb-3 text-light">About Me</h4>
 <p>
-  Hello, I'm Ahmet. Also known as "Kahverengi" in software and gaming world. A software developer and entrepreneur from Turkey. In
-  my daily life, I create and work on websites, mobile applications and
-  personal projects. In my free times, I develop myself in programming, I
-  try to learn and test new technologies. Mainly, I'm a back-end
-  developer.
+  Hello, I'm Ahmet. Also known as "Kahverengi" in software and gaming world. A
+  software developer and entrepreneur from Turkey. In my daily life, I create
+  and work on websites, mobile applications and personal projects. In my free
+  times, I develop myself in programming and trying new technologies. Mainly,
+  I'm a back-end developer.
 </p>
 
 <hr />
@@ -15,77 +14,77 @@
 <div class="row flex-wrap">
   <div class="col-auto">
     <div class="skill kotlin-bg">
-            <span>
-              Kotlin
-              <small class="text-muted">
-                Android
-                <br />
-                Web
-              </small>
-            </span>
+      <span>
+        Kotlin
+        <small class="text-muted">
+          Android
+          <br />
+          Web
+        </small>
+      </span>
     </div>
   </div>
   <div class="col-auto">
     <div class="skill js-bg">
-            <span>
-              Javascript
-              <small class="text-muted">
-                Svelte
-                <br />
-                Vue.js
-              </small>
-            </span>
+      <span>
+        Javascript
+        <small class="text-muted">
+          Svelte
+          <br />
+          Vue.js
+        </small>
+      </span>
     </div>
   </div>
   <div class="col-auto">
     <div class="skill">
-            <span>
-              Java
-              <small class="text-muted">
-                Desktop App
-                <br />
-                Mobile Navite
-              </small>
-            </span>
+      <span>
+        Java
+        <small class="text-muted">
+          Desktop App
+          <br />
+          Mobile Navite
+        </small>
+      </span>
     </div>
   </div>
   <div class="col-auto">
     <div class="skill">
-            <span>
-              Linux
-              <small class="text-muted">
-                <br />
-                Dev-Ops
-                <br />
-                Server Mang.
-              </small>
-            </span>
+      <span>
+        Linux
+        <small class="text-muted">
+          <br />
+          Dev-Ops
+          <br />
+          Server Mang.
+        </small>
+      </span>
     </div>
   </div>
   <div class="col-auto">
     <div class="skill">
-            <span>
-              Database
-              <small class="text-muted">
-                <br />
-                SQL techs,
-                <br />
-                Data mang.
-              </small>
-            </span>
+      <span>
+        Database
+        <small class="text-muted">
+          <br />
+          SQL techs,
+          <br />
+          Data mang.
+        </small>
+      </span>
     </div>
   </div>
   <div class="col-auto">
     <div class="skill">
-            <span>
-              Others
-              <small class="text-muted">
-                <br />
-                Git
-                <br />
-                Docker
-              </small>
-            </span>
+      <span>
+        Others
+        <small class="text-muted">
+          <br />
+          Git
+          <br />
+          Docker
+        </small>
+      </span>
     </div>
   </div>
 </div>
