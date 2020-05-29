70b63555b3bfd6081a989db4b1c7075af5ea5ef1
Selim Gökçek <selimgokcek@outlook.com>
2020-05-29 15:10:33 +0300

    Initial design

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,1 +1,179 @@
-<h1>Hello world!</h1>
+<div class="container">
+
+  <div class="row justify-content-center mt-4 py-5">
+    <div class="col-4">
+
+      <img
+        src="assets/img/pp.jpeg"
+        class="rounded border border-cream d-block m-auto p-1 mb-3"
+        width="120"
+        height="120"
+        alt="Ahmet Enes Duruer"
+      />
+    </div>
+    <div class="col-6 text-right">
+      <nav class="nav lead">
+        <a class="nav-link active pl-0" href="#">About</a>
+        <a class="nav-link" href="#">Resume</a>
+        <a class="nav-link" href="#">Works</a>
+        <a class="nav-link ml-auto" href="#">TR</a>
+      </nav>
+    </div>
+  </div>
+
+  <div class="row justify-content-center">
+
+    <div class="col-4 text-center position-relative">
+      <div class="pb-3">
+        <h4 class="font-weight-normal text-white">Ahmet Enes Duruer</h4>
+        <p>
+          Software Developer,
+          <br />
+          Entrepreneur
+        </p>
+      </div>
+      <ul class="list-unstyled pb-3">
+        <a href="#">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-gitlab"
+          >
+            <path
+              d="M22.65 14.39L12 22.13 1.35 14.39a.84.84 0 0 1-.3-.94l1.22-3.78
+              2.44-7.51A.42.42 0 0 1 4.82 2a.43.43 0 0 1 .58 0 .42.42 0 0 1
+              .11.18l2.44 7.49h8.1l2.44-7.51A.42.42 0 0 1 18.6 2a.43.43 0 0 1
+              .58 0 .42.42 0 0 1 .11.18l2.44 7.51L23 13.45a.84.84 0 0 1-.35.94z"
+            ></path>
+          </svg>
+        </a>
+        <a href="#" class="mx-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-github"
+          >
+            <path
+              d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0
+              0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07
+              5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65
+              5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0
+              5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+            ></path>
+          </svg>
+        </a>
+        <a class="mr-3" href="#">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-facebook"
+          >
+            <path
+              d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1
+              1-1h3z"
+            ></path>
+          </svg>
+        </a>
+        <a href="#">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-twitter"
+          >
+            <path
+              d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66
+              10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0
+              20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
+            ></path>
+          </svg>
+        </a>
+      </ul>
+      <button class="btn bg-lightglass text-primary d-block m-auto">
+        Contact Me
+      </button>
+
+      <small class="d-inline-block text-muted pt-5">
+        Maded with ❤ and
+        <a href="#">Svelte</a>
+        .
+      </small>
+      <div class="particle-1"></div>
+    </div>
+
+    <div class="col-6">
+
+      <h4 class="pb-3 text-light">About Me</h4>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis,
+        doloribus possimus corrupti sunt deleniti unde eos ad culpa dolorum illo
+        sit tempora debitis odio ut.
+      </p>
+      <hr />
+
+      <h3 class="py-3 text-light">Skills</h3>
+
+      <div class="row">
+        <div class="col-auto">
+          <div class="skill kotlin-bg">
+            <span>Kotlin</span>
+          </div>
+        </div>
+        <div class="col-auto">
+          <div class="skill js-bg">
+            <span>Javascript</span>
+          </div>
+        </div>
+        <div class="col-auto">
+          <div class="skill">
+            <span>Mobile</span>
+          </div>
+        </div>
+        <div class="w-100 py-3"></div>
+        <div class="col-auto">
+          <div class="skill">
+            <span>Other</span>
+          </div>
+        </div>
+        <div class="col-auto">
+          <div class="skill">
+            <span>Other</span>
+          </div>
+        </div>
+        <div class="col-auto">
+          <div class="skill">
+            <span>Other</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
