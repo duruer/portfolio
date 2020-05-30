2ac0cf401e3d5027ff2bb6dab21dd5ca8105c91e
Selim Gökçek <selimgokcek@outlook.com>
2020-05-30 19:34:04 +0300

    Change background color

--- src/pages/References.svelte
+++ src/pages/References.svelte
@@ -1,1 +1,10 @@
-References here bla bla bla
+<artice class="d-block mb-5">
+  <h4 class="mb-4 text-light">Experiences</h4>
+
+  <div class="row"></div>
+</artice>
+
+<article class="d-block mb-5">
+  <h4 class="mb-4 text-light">Projects</h4>
+
+</article>
