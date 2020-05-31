763fa79f63bcfd2c76a2255808163fe92fa7cc80
Selim Gökçek <selimgokcek@outlook.com>
2020-05-31 23:06:18 +0300

    Add experiences

--- src/pages/References.svelte
+++ src/pages/References.svelte
@@ -1,7 +1,55 @@
+<style>
+  li {
+    list-style: ;
+  }
+</style>
+
 <artice class="d-block mb-5">
   <h4 class="mb-4 text-light">Experiences</h4>
 
-  <div class="row"></div>
+  <div class="row mb-3">
+    <div class="col-3">
+      <li class="text-muted">2020 - Now</li>
+    </div>
+    <div class="col-9">
+      <a href="#" class="text-secondary">Company Name</a>
+      /
+      <i>UI/UX Designer, Project Head</i>
+    </div>
+  </div>
+
+  <div class="row mb-3">
+    <div class="col-3">
+      <li class="text-muted">2020 - Now</li>
+    </div>
+    <div class="col-9">
+      <a href="#" class="text-secondary">Company Name</a>
+      /
+      <i>UI/UX Designer, Project Head</i>
+    </div>
+  </div>
+
+  <div class="row mb-3">
+    <div class="col-3">
+      <li class="text-muted">2020 - Now</li>
+    </div>
+    <div class="col-9">
+      <a href="#" class="text-secondary">Company Name</a>
+      /
+      <i>UI/UX Designer, Project Head</i>
+    </div>
+  </div>
+
+  <div class="row mb-3">
+    <div class="col-3">
+      <li class="text-muted">2020 - Now</li>
+    </div>
+    <div class="col-9">
+      <a href="#" class="text-secondary">Company Name</a>
+      /
+      <i>UI/UX Designer, Project Head</i>
+    </div>
+  </div>
 </artice>
 
 <article class="d-block mb-5">
