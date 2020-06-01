22ae244b1ec472795c9d2e6962bf74e5cc111180
Selim Gökçek <selimgokcek@outlook.com>
2020-06-01 18:51:06 +0300

    Work page

--- src/pages/ProjectDetail.svelte
+++ src/pages/ProjectDetail.svelte
@@ -2,9 +2,9 @@
   export let project;
 </script>
 
-<artice class="d-block mb-5">
+<artice class="mb-5">
   <h4 class="mb-4 text-light">
-    <a href="#" class="text-primary">Project</a>
+    <a href="#">Project</a>
     ≫ Pano
   </h4>
   <p>
@@ -13,22 +13,25 @@
     incidunt impedit aut, rem at repellat repudiandae quas a!
   </p>
   <h6 class="text-muted">2018 - 2020 Present</h6>
-  <hr />
-  <ul class="list-unstyled d-flex flex-row">
-    <li class="pr-3">
+
+  <h5 class="my-4">Links</h5>
+  <ul class="list-group list-group-horizontal list-unstyled mb-3">
+    <li class="list-group-item bg-transparent pl-0">
       <a href="#">Website</a>
     </li>
-    <li class="pr-3">
+    <li class="list-group-item bg-transparent">
       <a href="#">Github</a>
     </li>
-    <li class="pr-3">
+    <li class="list-group-item bg-transparent">
       <a href="#">Gitlab</a>
     </li>
   </ul>
+
+  <h5 class="my-4">Screenshots</h5>
   <img
     src="../assets/img/pano-ss-1.png"
     class="border rounded img-fluid mb-3"
     alt="Project Image 1"
+    title="Project Image 1"
   />
-
 </artice>
--- src/pages/WorkDetail.svelte
+++ src/pages/WorkDetail.svelte
@@ -2,6 +2,69 @@
   export let work;
 </script>
 
-Work: {work}
-<br />
-Work details....
+<artice class="mb-5">
+  <h4 class="mb-4 text-light">
+    <a href="#">Work</a>
+    ≫ General Mobile Inc.
+  </h4>
+  <img
+    src="../assets/img/gm-logo.jpeg"
+    alt="General Mobile Inc."
+    class="rounded img-fluid"
+  />
+  <h5 class="my-4">Description</h5>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Non esse fugiat
+    corporis optio voluptatum a eveniet. Dolorum ex praesentium ducimus! Quasi
+    incidunt impedit aut, rem at repellat repudiandae quas a!
+  </p>
+  <ul class="list-inline text-muted">
+    <li class="mb-2">
+      <span class="mr-2 text-primary">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-briefcase"
+        >
+          <rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect>
+          <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
+        </svg>
+      </span>
+      as Android Developer, between 2018-1019
+    </li>
+    <li>
+      <span class="mr-2 text-primary">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-map-pin"
+        >
+          <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+          <circle cx="12" cy="10" r="3"></circle>
+        </svg>
+      </span>
+      İstanbul, Turkey
+    </li>
+  </ul>
+
+  <h5 class="my-4">Used Technologies</h5>
+  <ul class="list-inline">
+    <li>- Kotlin</li>
+    <li>- Java</li>
+  </ul>
+
+</artice>
