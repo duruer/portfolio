c236ca55465cb8471076bf80f4e3e0fd28eee09a
kahverengi <ahmet.duruer@yandex.com>
2020-06-02 15:44:29 +0300

    Add General Mobile work experience in both Turkish and English language

--- src/references.config.js
+++ src/references.config.js
@@ -1,8 +1,8 @@
 export default {
   works: {
     "general-mobile": {
-      "tr": {
-        companyName: "General Mobile Inc.",
+      tr: {
+        companyName: "General Mobile AŞ.",
         logoImage: "/assets/img/gm-logo-dark.jpeg",
         startDate: 2017,
         endDate: 2018,
@@ -10,10 +10,26 @@ export default {
         position: "Android Geliştiricisi",
         location: {
           city: "İstanbul",
-          country: "Turkey"
-        }
+          country: "Türkiye",
+        },
+        description:
+          "Stok Android uygulamaları üzerinde çalışıyorduk. Tasarımcı tarafından <a href='https://www.sketch.com' target='_blank'>Sketch</a>'te çizilen çizimleri Android'de UI yaptık." +
+          "Başlangıçta 6 kişilik bir geliştirici ekibiyle devam ettik. " +
+          "Ben bazı şirket uygulama projelerinde yer aldım. Bunlardan bazıları <a href='https://play.google.com/store/apps/details?id=com.generalmobile.app.gallery' target='_blank'>GM Gallery Go</a>, <a href='https://play.google.com/store/apps/details?id=com.generalmobile.app.musicplayer' target='_blank'>GM Music Player</a>, <a href='https://play.google.com/store/apps/details?id=com.generalmobile.assistant' target='_blank'>GM Assistant</a> ve <a href='https://play.google.com/store/apps/details?id=com.generalmobile.app.gmfilemanager' target='_blank'>GM Files</a>. " +
+          "Hala Google Play'de aktif bir şekilde bulunuyorlar. Tamamen Java ve Kotlin ile üretildiler. " +
+          "GitLab'taki issue takip etme sistemini kullandık.",
+        usedTechnologies: [
+          "Kotlin",
+          "Java",
+          "Gradle",
+          "Git",
+          "GitLab",
+          "Slack",
+          "Jenkins",
+          "Android Studio",
+        ],
       },
-      "en": {
+      en: {
         companyName: "General Mobile Inc.",
         logoImage: "/assets/img/gm-logo-dark.jpeg",
         startDate: 2017,
@@ -22,10 +38,26 @@ export default {
         position: "Android Developer",
         location: {
           city: "İstanbul",
-          country: "Turkey"
-        }
-      }
-    }
+          country: "Turkey",
+        },
+        description:
+          "We were working on and building stock Android applications. We were making UI in Android by design from <a href='https://www.sketch.com' target='_blank'>Sketch</a>. " +
+          "We were 6 members developer team in the beginning. " +
+          "I have worked on some of company application projects such as <a href='https://play.google.com/store/apps/details?id=com.generalmobile.app.gallery' target='_blank'>GM Gallery Go</a>, <a href='https://play.google.com/store/apps/details?id=com.generalmobile.app.musicplayer' target='_blank'>GM Music Player</a>, <a href='https://play.google.com/store/apps/details?id=com.generalmobile.assistant' target='_blank'>GM Assistant</a> and <a href='https://play.google.com/store/apps/details?id=com.generalmobile.app.gmfilemanager' target='_blank'>GM Files</a>. " +
+          "They Are still available in Google Play. They have built in completely Java and Kotlin. " +
+          "We have used issue tracking system in GitLab.",
+        usedTechnologies: [
+          "Kotlin",
+          "Java",
+          "Gradle",
+          "Git",
+          "GitLab",
+          "Slack",
+          "Jenkins",
+          "Android Studio",
+        ],
+      },
+    },
   },
   projects: {},
 };
