458dba07041f1a6f4a83e89787583cdeff2fca5f
kahverengi <ahmet.duruer@yandex.com>
2020-06-02 14:24:14 +0300

    Fix wrong link in bio (twitter -> linkedin)

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -60,7 +60,7 @@
     <div class="col d-lg-none">
       <h4 class="font-weight-normal text-white">Ahmet Enes Duruer</h4>
       <p class="mb-2">
-        {@html $_("bio.status")}
+        {@html $_('bio.status')}
       </p>
       <ul class="list-unstyled py-2">
         <a href="https://gitlab.com/kahverengi" target="_blank">
@@ -129,7 +129,10 @@
             ></path>
           </svg>
         </a>
-        <a href="#">
+        <a
+          href="https://linkedin.com/in/ahmet-enes-duruer-9bb3b554"
+          target="_blank"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="20"
@@ -140,13 +143,14 @@
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="feather feather-twitter"
+            class="feather feather-linkedin"
           >
             <path
-              d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66
-              10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0
-              20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
+              d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2
+              2v7h-4v-7a6 6 0 0 1 6-6z"
             ></path>
+            <rect x="2" y="9" width="4" height="12"></rect>
+            <circle cx="4" cy="4" r="2"></circle>
           </svg>
         </a>
       </ul>
@@ -155,38 +159,38 @@
           href="mailto:mail@mail.com"
           class="btn btn-outline-lightglass text-primary"
         >
-          {$_("bio.contact_button")}
+          {$_('bio.contact_button')}
         </a>
         <a href="#" class="btn btn-outline-lightglass text-light">
-          {$_("bio.download_cv_button")}
+          {$_('bio.download_cv_button')}
         </a>
       </div>
     </div>
     <div class="col-lg-6">
       <nav class="nav pt-lg-0 pt-4">
         <a class="nav-link pl-0" class:active="{$path === '/'}" href="/">
-          {$_("nav_links.about")}
+          {$_('nav_links.about')}
         </a>
         <a
           class="nav-link"
           class:active="{$path === '/references'}"
           href="/references"
         >
-          {$_("nav_links.references")}
+          {$_('nav_links.references')}
         </a>
         <a
           class="nav-link"
           href="https://github.com/kahverengi001/portfolio"
           target="_blank"
         >
-          {$_("nav_links.this_website")}
+          {$_('nav_links.this_website')}
         </a>
         <a
           class="nav-link ml-auto"
           href="javascript:void(0);"
           on:click="{() => onLocaleChangeClick()}"
         >
-          {$locale === "tr" ? "EN (US)" : "TR"}
+          {$locale === 'tr' ? 'EN (US)' : 'TR'}
         </a>
       </nav>
     </div>
@@ -197,7 +201,7 @@
     <div class="col-lg-4 text-center d-lg-block d-none">
       <h4 class="font-weight-normal text-white">Ahmet Enes Duruer</h4>
       <p class="mb-4">
-        {@html $_("bio.status")}
+        {@html $_('bio.status')}
       </p>
       <ul class="list-unstyled pb-3">
         <a href="https://gitlab.com/kahverengi" target="_blank">
@@ -296,15 +300,15 @@
           href="mailto:mail@mail.com"
           class="btn btn-outline-lightglass text-primary"
         >
-          {$_("bio.contact_button")}
+          {$_('bio.contact_button')}
         </a>
         <a href="#" class="btn btn-outline-lightglass text-light">
-          {$_("bio.download_cv_button")}
+          {$_('bio.download_cv_button')}
         </a>
       </div>
 
       <small class="d-inline-block text-muted pt-5">
-        {@html $_("bio.made_with")}
+        {@html $_('bio.made_with')}
       </small>
       <div class="particle-1"></div>
     </div>
