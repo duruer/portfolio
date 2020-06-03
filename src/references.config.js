07c9186ef6b3693d9c3827f7caa1aaee9271b5c0
kahverengi <ahmet.duruer@yandex.com>
2020-06-03 16:29:09 +0300

    Add portfolio project details

--- src/references.config.js
+++ src/references.config.js
@@ -64,76 +64,131 @@ export default {
     },
   },
   projects: {
-    "portfolio": {
+    portfolio: {
       tr: {
         logoImage: "/assets/img/references/projects/portfolio-tr.png",
         projectName: "Portfolio",
-        description: "Kendi portfolyo websitem."
+        description: "Kendi portfolyo websitem.",
+        startDate: 2020,
+        endDate: "",
+        detailsDescription:
+          "Kendimi tanıtmak için oluşturmuş olduğum küçük portfolyo websitem. Bu projede geliştirilirken arayüz kodlaması" +
+          " için <a href='https://svelte.dev' target='_blank'>Svelte 3</a>, Svelte 3'te routing için kendi geliştirmiş olduğum <a href='https://github.com/routve/routve' target='_blank'>Routve</a> kütüphanesi, " +
+          "CSS kodlamak için <a href='https://sass-lang.com' target='_blank'>SASS</a>, " +
+          "proje bundler'ı olarak <a href='https://rollupjs.org' target='_blank'>RollupJS</a>, versiyon kontrol sistemi olarak <a href='https://github.com' target='_blank'>GitHub (Git)</a>, " +
+          "daha fazla tarayıcı desteği için <a href='https://babeljs.io' target='_blank'>Babel JS</a>, kod formatlayıcısı olarak <a href='https://prettier.io/' target='_blank'>Prettier</a>," +
+          "build için <a href='https://travis-ci.com' target='_blank'>Travis CI</a>, deploy ve yayın için <a href='https://heroku.com' target='_blank'>Heroku</a> teknolojileri kullanıldı.",
+        links: [
+          {
+            name: "Website",
+            address: "https://ahmetduruer.com",
+          },
+          {
+            name: "GitHub",
+            address: "https://github.com/kahverengi001/portfolio",
+          },
+        ],
+        images: [
+          {
+            title: "Kahverengi Portfolio",
+            address: "/assets/img/references/projects/portfolio-tr.png",
+          },
+        ],
       },
       en: {
         logoImage: "/assets/img/references/projects/portfolio-en.png",
         projectName: "Portfolio",
-        description: "My own portfolio website."
+        description: "My own portfolio website.",
+        startDate: 2020,
+        endDate: "",
+        detailsDescription:
+          "My small portfolio website that I created to introduce myself. In this project while in development, these technologies has used in, <a href='https://svelte.dev' target='_blank'>Svelte 3</a> for coding of UI, " +
+          "for router and routing the project I have created for Svelte 3 which is <a href='https://github.com/routve/routve' target='_blank'>Routve</a> library, " +
+          "<a href='https://sass-lang.com' target='_blank'>SASS</a> for coding CSS, " +
+          "<a href='https://rollupjs.org' target='_blank'>RollupJS</a> as project bundler, <a href='https://github.com' target='_blank'>GitHub (Git)</a> as version controlling system, " +
+          "<a href='https://babeljs.io' target='_blank'>Babel JS</a> for more browser support, <a href='https://prettier.io/' target='_blank'>Prettier</a> as code formatter, " +
+          "<a href='https://travis-ci.com' target='_blank'>Travis CI</a> for build up project and <a href='https://heroku.com' target='_blank'>Heroku</a> for deploy.",
+        links: [
+          {
+            name: "Website",
+            address: "https://ahmetduruer.com",
+          },
+          {
+            name: "GitHub",
+            address: "https://github.com/kahverengi001/portfolio",
+          },
+        ],
+        images: [
+          {
+            title: "Kahverengi Portfolio",
+            address: "/assets/img/references/projects/portfolio-en.png",
+          },
+        ],
       },
     },
-    "routve": {
+    routve: {
       tr: {
         logoImage: "/assets/img/references/projects/routve-logo.png",
         projectName: "Routve",
-        description: "Gelişmiş bir <a href='https://svelte.dev' target='_blank'>Svelte 3</a> router kütüphanesi."
+        description:
+          "Gelişmiş bir <a href='https://svelte.dev' target='_blank'>Svelte 3</a> router kütüphanesi.",
       },
       en: {
         logoImage: "/assets/img/references/projects/routve-logo.png",
         projectName: "Routve",
-        description: "An advanced <a href='https://svelte.dev' target='_blank'>Svelte 3</a> router library."
+        description:
+          "An advanced <a href='https://svelte.dev' target='_blank'>Svelte 3</a> router library.",
       },
     },
-    "pano": {
+    pano: {
       tr: {
         logoImage: "/assets/img/references/projects/pano-logo.png",
         projectName: "Pano",
-        description: "Minecraft sunucunuzun website ihtiyaçlarını karşılayan gelişmiş bir web platform."
+        description:
+          "Minecraft sunucunuzun website ihtiyaçlarını karşılayan gelişmiş bir web platform.",
       },
       en: {
         logoImage: "/assets/img/references/projects/pano-logo.png",
         projectName: "Pano",
-        description: "An advanced web platform that comfort your Minecraft server's website needs."
+        description:
+          "An advanced web platform that comfort your Minecraft server's website needs.",
       },
     },
     "antik-kup": {
       tr: {
         logoImage: "/assets/img/references/projects/ak-logo.png",
         projectName: "Antik Küp",
-        description: "Türkçe Minecraft MMORPG topluluk sunucusu."
+        description: "Türkçe Minecraft MMORPG topluluk sunucusu.",
       },
       en: {
         logoImage: "/assets/img/references/projects/ak-logo.png",
         projectName: "Antik Küp",
-        description: "A complete Turkish Minecraft MMORPG community server."
+        description: "A complete Turkish Minecraft MMORPG community server.",
       },
     },
     "sosyal-okul": {
       tr: {
         logoImage: "/assets/img/references/projects/sosyal-okul-image.jpg",
         projectName: "Sosyal Okul",
-        description: "Okullar için tasarlanmış bir sosyal medya paylaşım sitesi."
+        description:
+          "Okullar için tasarlanmış bir sosyal medya paylaşım sitesi.",
       },
       en: {
         logoImage: "/assets/img/references/projects/sosyal-okul-image.jpg",
         projectName: "Social School",
-        description: "A social networking website designed for schools."
+        description: "A social networking website designed for schools.",
       },
     },
-    "papuaya": {
+    papuaya: {
       tr: {
         logoImage: "/assets/img/references/projects/papuaya-image.png",
         projectName: "Papuaya",
-        description: "Bir sosyal medya paylaşım sitesi."
+        description: "Bir sosyal medya paylaşım sitesi.",
       },
       en: {
         logoImage: "/assets/img/references/projects/papuaya-image.png",
         projectName: "Papuaya",
-        description: "A social networking website."
+        description: "A social networking website.",
       },
     },
   },
