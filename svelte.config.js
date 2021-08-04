4e1658bcdf815a36bacba8d6226a59378df00702
kahverengi <ahmet.duruer@yandex.com>
2021-08-04 10:32:17 +0300

    Fix sass warning issue and upgrade Svelte Kit

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,13 @@
+import sveltePreprocess from "svelte-preprocess";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: sveltePreprocess({
+		scss: {
+			quietDeps: true
+		}
+	}),
+
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
