e47f1f89b27969387f53a069615177cf066a8333
kahverengi <ahmet.duruer@yandex.com>
2021-08-06 16:53:53 +0300

    Add error page title

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,4 +1,7 @@
-4
+<svelte:head>
+  <title>{status} | {$_("page_title")}</title>
+</svelte:head>
+
 <h4 class="text-primary">404</h4>
 {$_("pages.error_404.message")}
 
@@ -16,4 +19,6 @@
 
 <script>
   import { _ } from "svelte-i18n";
+
+  export let status;
 </script>
