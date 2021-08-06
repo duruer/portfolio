a48508de30a9ecbce417fb54c8a516f8eafa81af
kahverengi <ahmet.duruer@yandex.com>
2021-08-06 12:38:54 +0300

    Add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,19 @@
+4
+<h4 class="text-primary">404</h4>
+{$_("pages.error_404.message")}
+
+<script context="module">
+  export function load({ error, status }) {
+    return {
+      props: {
+        status,
+        message: error.message,
+        title: `${status}: ${error.message}`,
+      },
+    };
+  }
+</script>
+
+<script>
+  import { _ } from "svelte-i18n";
+</script>
