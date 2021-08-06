21652ea38fde94b51eef24f017d583e966e85fb4
kahverengi <ahmet.duruer@yandex.com>
2021-08-06 12:34:58 +0300

    Upgrade Svelte Kit and packages

--- svelte.config.js
+++ svelte.config.js
@@ -2,16 +2,16 @@ import sveltePreprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: sveltePreprocess({
-		scss: {
-			quietDeps: true
-		}
-	}),
+  preprocess: sveltePreprocess({
+    scss: {
+      quietDeps: true,
+    },
+  }),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+  },
 };
 
 export default config;
