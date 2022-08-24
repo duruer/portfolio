b5089f0a12d66dc4c923389cfd290902cacfa74b
kahverengi <ahmet@duruer.dev>
2022-08-24 16:36:45 +0300

    Remove useless hooks file

--- src/hooks.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@sveltejs/kit').GetSession} */
-export function getSession(request) {
-  const acceptedLanguage =
-    typeof request.headers["accept-language"] === "undefined"
-      ? ""
-      : request.headers["accept-language"].split(",")[0];
-
-  return { acceptedLanguage };
-}
